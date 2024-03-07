d4996ea1748509f2956bc6df36db87f14b8e06b2
Body1616 <161995851+Body1616@users.noreply.github.com>
2024-03-07 13:55:10 +0800

    Add files via upload
    first commit

---
+++ Script/script.js
@@ -0,0 +1,8 @@
+// script.js
+function submitForm() {
+    // You can perform any actions or connect to another file here
+    alert("Form submitted!"); // Example: show an alert
+
+    // If you want to redirect to another page, you can use window.location.href
+    window.location.href = "facilities.php";
+}
